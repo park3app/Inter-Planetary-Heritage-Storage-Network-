5aae2dfa870e71c0695d192e2fba5be840377eca
shashank lalit patil <shashankpatil125@gmail.com>
2023-06-17 07:04:37 +0000

    single proposal prompt done

--- src/Components/Proposals/SuccessSingleProposal/SuccessSingleProposal.js
+++ src/Components/Proposals/SuccessSingleProposal/SuccessSingleProposal.js
@@ -130,25 +130,25 @@ const SuccessSingleProposal = () => {
   </Center> 
   :
 
-  <div className='asset-details-div'>
+  <div className='px-28 py-20 bg-[#0a1930]'  >
 
-  <HStack spacing={8} >
-    {/* <Link  to={tokenuri} target='_blank' > */}
-    <Image src={image} alt={name}  fallbackSrc={screenshot} maxW={'40%'} />
-    {/* </Link> */}
+<HStack spacing={8} className='bg-[#17173d] shadow-xl shadow-black rounded-2xl p-8'>
+<div className='w-6/12 h-full'>
+              <img className="border-cyan-500 border-2 w-full mx-auto rounded-xl" src={`${image.replace('ipfs://', 'https://nftstorage.link/ipfs/')}`} alt={name} fallbackSrc={screenshot} style={{ maxWidth: '60%' }} />
+              </div>
     
-      <VStack spacing={6}   align='stretch' marginLeft={'5rem'}>
+              <VStack spacing={6} className='text-[#b1aeae]' align='stretch' marginLeft={'5rem'}>
       <div className='details-div'>
       <Heading as="h3"  m={'1'} size="lg" >
         #{id}<Link style={{marginLeft:'3px'}} target='_blank' to={tokenuri}><ExternalLinkIcon/></Link>
       </Heading>
-      <Heading as="h6" m={'1'} size="lg" fontWeight={'1000'} color={'rgba(0, 0, 0, 0.53)'}>
+      <Heading as="h6" m={'1'} size="lg" fontWeight={'1000'} color={'white'}>
         {name.toUpperCase()}
       </Heading>
-      <Text  color={'rgba(0, 0, 0, 0.53)'} fontWeight={'700'} m={'1'} fontSize={'xl'}>{description}</Text>
-      <Text  color={'rgba(0, 0, 0, 0.53)'} fontWeight={'700'} m={'1'} fontSize={'md'}>Culutural significance:   {significance}</Text>
-      <Text fontSize="2xl" m={'1'} color={'rgba(0, 0, 0, 0.53)'} fontWeight={'600'}>{`${location.charAt(0).toUpperCase()}${location.slice(1)}`}</Text>
-      <Text fontSize="md" color={'rgba(0, 0, 0, 0.53)'} fontWeight={'400'} m={'1'}>{owner}</Text>
+      <Text   fontWeight={'700'} m={'1'} fontSize={'xl'}>{description}</Text>
+      <Text   fontWeight={'700'} m={'1'} fontSize={'md'}>Culutural significance:   {significance}</Text>
+      <Text fontSize="2xl" m={'1'}  fontWeight={'600'}>{`${location.charAt(0).toUpperCase()}${location.slice(1)}`}</Text>
+      <Text fontSize="md"  fontWeight={'400'} m={'1'}>{owner}</Text>
       </div>
 
      <HStack>
